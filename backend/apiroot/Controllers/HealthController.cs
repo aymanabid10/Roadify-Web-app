20dcc82a4ce55464978dcee8172e7218581eae0e
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2026-01-22 16:39:04 +0100

    Enforce authorization on HealthController

--- backend/apiroot/Controllers/HealthController.cs
+++ backend/apiroot/Controllers/HealthController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace apiroot.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("api/[controller]")]
 public class HealthController : ControllerBase
 {
