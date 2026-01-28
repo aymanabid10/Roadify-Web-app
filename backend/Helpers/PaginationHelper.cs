2c75c53f0bda069292880b8b4472553688d254a0
Aymen Abid <aymen.abid2023@gmail.com>
2026-01-28 07:30:21 +0100

    feat : Create PaginationHelper to reuse in all CRUD Operations

---
+++ backend/Helpers/PaginationHelper.cs
@@ -0,0 +1,33 @@
+using apiroot.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+public static class PaginationHelper
+{
+    public static async Task<PaginatedResponse<T>> PaginateAsync<T>(
+        IQueryable<T> query,
+        int page,
+        int pageSize)
+    {
+        if (page <= 0) page = 1;
+        if (pageSize <= 0) pageSize = 10;
+
+        var totalCount = await query.CountAsync();
+        var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+
+        var data = await query
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return new PaginatedResponse<T>
+        {
+            Data = data,
+            Page = page,
+            PageSize = pageSize,
+            TotalCount = totalCount,
+            TotalPages = totalPages,
+            HasPrevious = page > 1,
+            HasNext = page < totalPages
+        };
+    }
+}
