22261d25ecefc771e6b7c72787d929b2c48fe986
elyes-ST <elyes.mlawah@singleton.tn>
2026-01-29 10:35:58 +0100

    correct Guid

--- backend/apiroot/DTOs/ListingDtos.cs
+++ backend/apiroot/DTOs/ListingDtos.cs
@@ -24,7 +24,7 @@ public abstract class CreateListingRequestBase
     public string? ContactPhone { get; set; }
 
     [Required]
-    public int VehicleId { get; set; }
+    public Guid VehicleId { get; set; }
 
     [Required]
     [MaxLength(100)]
